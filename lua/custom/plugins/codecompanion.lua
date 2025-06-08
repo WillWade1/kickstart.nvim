51b531210277ab4e5b73f07e7c3e23bfd018ba5a
Will <williamwade1000@btinternet.com>
2025-06-08 19:19:35 +0100

    Added keymaps, mini-diff and markdown plugin for codecompanion

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -19,5 +19,30 @@ return {
         },
       }
     end,
+    keys = {
+      { '<leader>ka', '<cmd>CodeCompanion<cr>', desc = 'Code Companion' },
+      { '<leader>ks', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Code Companion Chat' },
+      { '<leader>kl', '<cmd>CodeCompanionInline<cr>', desc = 'Code Companion Inline' },
+    },
+  },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { 'markdown', 'codecompanion' },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  {
+    'echasnovski/mini.diff',
+    config = function()
+      local diff = require 'mini.diff'
+      diff.setup {
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      }
+    end,
   },
 }
